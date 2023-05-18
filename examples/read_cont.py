53fa4cb4a629160ce85c738a18ffcc32dfed4e04
Anil Gurses <anilgurses98@gmail.com>
2023-05-18 13:05:39 -0400

    Update example

--- examples/read_cont.py
+++ examples/read_cont.py
@@ -4,9 +4,8 @@ from time import sleep
 dev = base.PowerMeter()
 # List available devices
 devs = dev.get_device_list()
-
 if len(devs) > 0:
-    dev.open_device(devs(0))
+    dev.open_device(devs[0])
 else:
     print("No device found!")
     exit(1)
@@ -15,7 +14,7 @@ print("Available devices:")
 print(devs)
 
 # Setting frequency to 20 GHz
-dev.frequency(20e9)
+dev.frequency = 1.85e9
 
 while True:
     # Read power level every second
